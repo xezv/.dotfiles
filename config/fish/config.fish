28590467cc43381323aafd04c3e6c8f2fc46b77c
xezv <xezv@users.noreply.github.com>
2024-10-16 11:59:18 +0900

    .

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,21 @@
+set -a bass_files /etc/profile.d/*.sh ~/.config/fish/*.sh
+
 if status is-interactive
-    bass source /etc/profile.d/*.sh
-    bass source ~/.config/fish/*.sh 
+    if not type -q bass
+        echo "not found bass. install bass(config.fish)"
+        exit 1
+    end
+
+    for file in $bass_files
+        if not test -e $file
+            echo "not found $file(config.fish)"
+            exit 1
+        end
+        bass source $file
+        if [ $status -ne 0 ]
+            echo "failed to source $file(config.fish)"
+            exit 1
+        end
+    end
+  
 end
